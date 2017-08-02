fe8cbbb1e8ee6de7147b1652c9386f2d3fbe89f3
Evan Tucker <eej.tucker@gmail.com>
2017-08-02 11:07:30 -0600

    added scrolling links, and contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,18 @@ $( document ).ready(function(){
 $(".button-collapse").sideNav();
 $('.parallax').parallax();
 
+// scroll down page to the about me section
+$("#aboutMe").on("click", function() {
+	$("html, body").animate({ scrollTop: "370px" }, "slow");
+})
+
+// scroll down page to the contact section
+$("#contact").on("click", function() {
+	$("html, body").animate({ scrollTop: "1900px" }, "slow");
+})
+
+$("#submit").on("click", function() {
+	event.preventDefault();
+})
+
 });

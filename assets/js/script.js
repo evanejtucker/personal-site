a23903e549589d1bfc8a6fb5280a5013f14e4aed
Evan Tucker <eej.tucker@gmail.com>
2017-08-03 16:49:34 -0600

    added portfolio cards, fixed nav scroll functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,27 @@ $( document ).ready(function(){
 
 $(".button-collapse").sideNav();
 $('.parallax').parallax();
+// $('.carousel').carousel();
+$('.carousel.carousel-slider').carousel({fullWidth: true}, "next");
 
-// scroll down page to the about me section
-$("#aboutMe").on("click", function() {
-	$("html, body").animate({ scrollTop: "370px" }, "slow");
-})
 
-// scroll down page to the contact section
-$("#contact").on("click", function() {
-	$("html, body").animate({ scrollTop: "1900px" }, "slow");
-})
+	// scroll down page to the about me section
+	$("#aboutMe").on("click", function() {
+		$("html, body").animate({ scrollTop: $("#scrollAboutMe").offset().top }, "slow");
+	});
 
-$("#submit").on("click", function() {
-	event.preventDefault();
-})
+	// scroll down page to the about me section
+	$("#portfolio").on("click", function() {
+		$("html, body").animate({ scrollTop: $("#scrollPortfolio").offset().top }, "slow");
+	});
+
+	// scroll down page to the contact section
+	$("#connect").on("click", function() {
+		$("html, body").animate({ scrollTop: $("#scrollContact").offset().top }, "slow");
+	});
+
+	$("#submit").on("click", function() {
+		event.preventDefault();
+	});
 
 });
